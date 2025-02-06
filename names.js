9b493e9ae32a95b7fd766fec5c1eff970aba16f4
Sebastiano-Pizzata <rsebastiano23@gmail.com>
2025-02-06 12:54:32 +0100

    Add: function names

---
+++ names.js
@@ -0,0 +1,16 @@
+// 1 - names.js
+// 1. Crea una funzione che accetta due parametri: firstName, lastName. 
+// La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+// 2. Esporta la funzione dal file.
+
+
+function names(firstName, lastName) {
+    return {
+        nome: firstName,
+        cognome: lastName
+    }
+}
+
+export {
+    names
+}
